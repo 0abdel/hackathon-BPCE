48f7990022cac99285bbe08768fe9b870efe1256
Romain Hirigoyen <romain.hirigoyen@epitech.eu>
2025-04-16 22:17:08 +0200

    remove old backend

--- backend/routes/changementsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const changementController = require("../controllers/changementControllers");
-
-router.get("/", changementController.getAllChangements);
-router.get("/:id", changementController.getChangementById);
-
-module.exports = router;
--- backend/routes/directionRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const directionController = require("../controllers/directionControllers");
-
-router.get("/", directionController.getAllDirection);
-router.get("/:name", directionController.getDirectionByName);
-
-module.exports = router;
