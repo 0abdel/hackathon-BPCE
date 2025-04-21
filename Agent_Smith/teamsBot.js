985e815b5abcac72f817ad4d046d186d9d7b4402
Romain Hirigoyen <romain.hirigoyen@epitech.eu>
2025-04-21 18:22:41 +0200

    fix gémini version

--- Agent_Smith/teamsBot.js
+++ Agent_Smith/teamsBot.js
@@ -144,7 +144,7 @@ class TeamsBot extends TeamsActivityHandler {
 
 
             const response = await fetch(
-                `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-pro:generateContent?key=${GEMINI_API_KEY}`,
+                `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${GEMINI_API_KEY}`,
                 {
                     method: "POST",
                     headers: { "Content-Type": "application/json" },
